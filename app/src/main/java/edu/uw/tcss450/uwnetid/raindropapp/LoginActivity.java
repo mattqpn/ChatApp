3e0821608ef63f4a96645da1d8f61d65b478adbf
mattqpn <116048461+mattqpn@users.noreply.github.com>
2022-11-18 22:03:33 -0800

    added pushy code to LoginActivity

--- app/src/main/java/edu/uw/tcss450/uwnetid/raindropapp/LoginActivity.java
+++ app/src/main/java/edu/uw/tcss450/uwnetid/raindropapp/LoginActivity.java
@@ -1,15 +1,27 @@
 package edu.uw.tcss450.uwnetid.raindropapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import edu.uw.tcss450.uwnetid.raindropapp.model.PushyTokenViewModel;
+import me.pushy.sdk.Pushy;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //If it is not already running, start the Pushy listening service
+        Pushy.listen(this);
+        initiatePushyTokenRequest();
+    }
+
+    private void initiatePushyTokenRequest() {
+        new ViewModelProvider(this).get(PushyTokenViewModel.class).retrieveToken();
     }
 }
